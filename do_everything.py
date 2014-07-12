abb91f0be343daa626e12d4f41a861984a4660dd
David R. MacIver <david@drmaciver.com>
2014-07-12 12:38:58 +0200

    add a parent script that just invokes everythign with my desired values

---
+++ do_everything.py
@@ -0,0 +1,73 @@
+"""
+This script is superspecific to me and just runs all the scripts in here in the
+manner I actually use them. You're welcome to take this verbatim if you like
+but you're more likely to want to treat it as example usage.
+
+Copyright (C) 2014 David R. MacIver
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import argparse
+import feedthebee
+import backpressure
+import truncatedgoals
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='My beeminder automation'
+    )
+    parser.add_argument('--auth-token', type=str, required=True,
+                        help='The API key to use for beeminder')
+    parser.add_argument('--skip-feed', default=False,
+                        action='store_true',
+                        help="Don't update the blogging goal")
+    parser.add_argument('--skip-bedtime', default=False,
+                        action='store_true',
+                        help="Don't update the early nights goal")
+    parser.add_argument('--skip-backpressure', default=False,
+                        action='store_true',
+                        help="Don't update the backpressure goal")
+
+    args = parser.parse_args()
+    auth_token = args.auth_token
+
+    if not args.skip_feed:
+        feedthebee.sync_feed(
+            auth_token=auth_token,
+            goal='blogging',
+            feed="http://www.drmaciver.com/feed"
+        )
+    if not args.skip_backpressure:
+        backpressure.apply_backpressure(
+            auth_token=auth_token,
+            target_goal="backpressure",
+            max_days=7
+        )
+    if not args.skip_bedtime:
+        truncatedgoals.update_truncated_goal(
+            auth_token=auth_token,
+            source_goal="bedtime",
+            target_goal="earlynights",
+            threshold=10.5,
+        )
+
+if __name__ == '__main__':
+    main()
